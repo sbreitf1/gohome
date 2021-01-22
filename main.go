a8276a388e29591880d6803e5e57b21fd47a3b16
danielb <git@masterunit.net>
2021-01-22 14:30:42 +0100

    desktop notification on leave times

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/alecthomas/kingpin"
+	"github.com/danielb42/goat"
 	"github.com/sbreitf1/go-console"
 )
 
@@ -16,6 +17,7 @@ var (
 	argBreakTime  = appMain.Flag("break", "Ignore actual break time and take input like '00:45' instead").Short('b').String()
 	argColleagues = appMain.Flag("colleagues", "Show which colleagues are currently here.").Short('c').Bool()
 	argPGPKeyName = appMain.Flag("keyname", "Name/ID of GPG key to decrypt hosts-credentials file (deprecated)").Short('k').String()
+	argReminder   = appMain.Flag("reminder", "show desktop notification on target time").Short('r').Bool()
 )
 
 const (
@@ -198,6 +200,12 @@ func process() error {
 		console.Printlnf("10:00 at %s %s(%s break)%s", t4.Format("15:04"), colorBreakInfo, formatDurationMinutes(breakTime4), colorEnd)
 		console.Println("-------------------------------------------")
 		console.Printlnf("go home (%s) at %s%s%s %s(%s break)%s", formatDurationMinutes(targetTime), colorLeaveTime, t2.Format("15:04"), colorEnd, colorBreakInfo, formatDurationMinutes(breakTime2), colorEnd)
+
+		if *argReminder {
+			goat.ClearQueue("g")
+			goat.AddJob("notify-send -i error 'Go home!'", t2, "g")
+			goat.AddJob("notify-send -i error '10h-Limit in 15 min! GO HOME!'", t4.Add(-15*time.Minute), "g")
+		}
 	}
 
 	//TODO print warning "nicht eingestochen" in red
