850ce2e3609bd6359f96615e6326ecaad65d36d7
Simon Breitfelder <breitfelder@denic.de>
2025-03-07 14:17:24 +0100

    updated deb package

--- distrib/deb/package.sh
+++ distrib/deb/package.sh
@@ -2,24 +2,39 @@
 
 set -e
 
-VERSION=$(git describe --exact-match --tags HEAD | sed 's/v//g')
+VERSION=$1
+
 if [[ -z $VERSION ]]; then
-    echo "no version detected! git tag like 'v0.0.0' on current commit is required."
+    VERSION=$(git describe --exact-match --tags HEAD | sed 's/v//g')
+    if [[ -z $VERSION ]]; then
+        echo "no version detected! git tag like 'v0.0.0' on current commit is required."
+        exit 1
+    fi
+fi
+
+if [[ ! "$VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "invalid version! must be like '0.0.0'."
     exit 1
 fi
+
 echo "package version v${VERSION}"
 
 echo "compile application"
 mkdir -p ./gohome/usr/bin
-CGO_ENABLED=0 go build -o ./gohome/usr/bin/gohome ../../
+CGO_ENABLED=0 GOARCH=amd64 go build -o ./gohome/usr/bin/gohome ../../
+
+INST_SIZE=$(du -ks ./gohome/usr | cut -f 1)
 
 echo "prepare package files"
 mkdir -p ./gohome/DEBIAN
 echo "Package: gohome
 Version: ${VERSION}
 Maintainer: sbreitf1
-Architecture: all
-Description: Calculates your leave times from Matrix entries." > ./gohome/DEBIAN/control
+Architecture: amd64
+Installed-Size: $INST_SIZE
+Homepage: https://github.com/sbreitf1/gohome
+Description: Calculates your leave time from Matrix entries." > ./gohome/DEBIAN/control
 
-dpkg-deb --build gohome
-mv gohome.deb gohome-${VERSION}.deb
+dpkg-deb --build gohome gohome_${VERSION}_amd64.deb
+
+rm -rf ./gohome
