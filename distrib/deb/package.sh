53f40df3809f739d47e91d4b6a8b9d8fc53877ff
Simon Breitfelder <breitfelder@denic.de>
2025-03-07 07:31:52 +0100

    add deb package files

---
+++ distrib/deb/package.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+VERSION=$(git describe --tags HEAD | sed 's/v//g')
+if [[ -z $VERSION ]]; then
+    echo "no version detected!"
+    exit 1
+fi
+echo "package version v${VERSION}"
+
+echo "compile application"
+mkdir -p ./gohome/usr/bin
+CGO_ENABLED=0 go build -o ./gohome/usr/bin/gohome ../../
+
+echo "prepare package files"
+mkdir -p ./gohome/DEBIAN
+echo "Package: gohome
+Version: ${VERSION}
+Maintainer: sbreitf1
+Architecture: all
+Description: Calculates your leave times from Matrix entries." > ./gohome/DEBIAN/control
+
+dpkg-deb --build gohome
+mv gohome.deb gohome-${VERSION}.deb
